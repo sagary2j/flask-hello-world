b853007eae2a5ca3210cd31db4b4b1dce1f0dd83
Sagar R <sagar.ravkhande@gmail.com>
2024-07-04 15:25:01 +0200

    added terraform code

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,32 +1,32 @@
-#Creation of the ECR repo
-resource "aws_ecr_repository" "ecr" {
-  name = "flask-docker-app"
+# #Creation of the ECR repo
+# resource "aws_ecr_repository" "ecr" {
+#   name = "flask-docker-app"
 
-}
+# }
 
-#The ECR policy describes the management of images in the repo
-resource "aws_ecr_lifecycle_policy" "ecr_policy" {
-  repository = aws_ecr_repository.ecr.name
-  policy     = local.ecr_policy
-}
+# #The ECR policy describes the management of images in the repo
+# resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+#   repository = aws_ecr_repository.ecr.name
+#   policy     = local.ecr_policy
+# }
 
-#This is the policy defining the rules for images in the repo
-locals {
-  ecr_policy = jsonencode({
-    "rules" : [
-      {
-        "rulePriority" : 1,
-        "description" : "Expire images older than 14 days",
-        "selection" : {
-          "tagStatus" : "any",
-          "countType" : "sinceImagePushed",
-          "countUnit" : "days",
-          "countNumber" : 14
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      }
-    ]
-  })
-}
+# #This is the policy defining the rules for images in the repo
+# locals {
+#   ecr_policy = jsonencode({
+#     "rules" : [
+#       {
+#         "rulePriority" : 1,
+#         "description" : "Expire images older than 14 days",
+#         "selection" : {
+#           "tagStatus" : "any",
+#           "countType" : "sinceImagePushed",
+#           "countUnit" : "days",
+#           "countNumber" : 14
+#         },
+#         "action" : {
+#           "type" : "expire"
+#         }
+#       }
+#     ]
+#   })
+# }
